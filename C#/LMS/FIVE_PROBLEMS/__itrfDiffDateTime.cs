e9fe11360898156a94fc73a805e9c7d5f9cf0ba6
Kevin <parade358@naver.com>
2024-03-15 11:43:11 +0900

    __itrfDiffDateTime.cs [2024.03.15]

---
+++ C#/LMS/FIVE_PROBLEMS/__itrfDiffDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIVE_PROBLEMS
+{
+    internal interface __itrfDiffDateTime
+    {
+        double GetMinutes();
+        double GetHour();
+        double GetDay();
+        double GetMonth();
+    }
+}
