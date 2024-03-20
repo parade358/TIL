54e51c5d7fa654470e79ea8e96d061d9a0d4c59e
Kevin <parade358@naver.com>
2024-03-20 18:03:07 +0900

    Calculator [2024.03.20]
    계산기 값 가져오기까지 완성

--- C#/LMS/Calculator/frmCalculator.cs
+++ C#/LMS/Calculator/frmCalculator.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Calculator
 {
@@ -17,6 +18,58 @@ namespace Calculator
             InitializeComponent();
         }
 
- 
+        string test;
+
+        private void clickNumber(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            String expressionText = expressionBox.Text;
+            string lastChar = expressionText[expressionText.Length - 1].ToString();
+
+            if (expressionBox.Text.StartsWith("0"))
+            {
+                expressionBox.Text = clickedButton.Text;
+            }
+            else
+            {
+                if(lastChar != "+" && lastChar != "-" && lastChar != "*" && lastChar != "/")
+                {
+                    expressionBox.Text += clickedButton.Text;
+                }
+                else
+                {
+                    expressionBox.Text = clickedButton.Text;
+                }
+            }
+        }
+
+        private void clickClearBtn(object sender, EventArgs e)
+        {
+            resultBox.Text = "0";
+
+            expressionBox.Text = "0";
+
+            test = "asd";
+        }
+
+        private void clickOperatorBtn(object sender, EventArgs e)
+        {
+            String expressionText = expressionBox.Text;
+            Button clickedButton = sender as Button;
+            string lastChar = expressionText[expressionText.Length-1].ToString();
+
+            if (expressionBox.Text.StartsWith("0"))
+            {
+                expressionBox.Text = "0";
+            }
+            else
+            {
+                if (lastChar != "+" && lastChar != "-" && lastChar != "*" && lastChar != "/")
+                {
+                    expressionBox.Text += clickedButton.Text;
+                    resultBox.Text = test;
+                }
+            }
+        }
     }
 }
