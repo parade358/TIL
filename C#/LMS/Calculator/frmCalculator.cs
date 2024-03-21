0a22e7c2be0aec31f1aea5d6e185ab795f99f6ef
Kevin <parade358@naver.com>
2024-03-21 14:53:21 +0900

    Calculator HardCoding [2024.03.21]

--- C#/LMS/Calculator/frmCalculator.cs
+++ C#/LMS/Calculator/frmCalculator.cs
@@ -13,44 +13,100 @@ namespace Calculator
 {
     public partial class frmCalculator : Form
     {
+
+
+
+
+
+
+
         public frmCalculator()
         {
             InitializeComponent();
         }
 
-        string test;
+
+
+
+
+
+
+
+        private String num1;
+        private String num2;
+        private String elemMath;
+
+
+
+
+
+
+
+
 
         private void clickNumber(object sender, EventArgs e)
         {
             Button clickedButton = sender as Button;
-            String expressionText = expressionBox.Text;
-            string lastChar = expressionText[expressionText.Length - 1].ToString();
+            string lastChar = expressionBox.Text[expressionBox.Text.Length - 1].ToString();
 
             if (expressionBox.Text.StartsWith("0"))
             {
-                expressionBox.Text = clickedButton.Text;
+                expressionBox.Text += clickedButton.Text;
             }
             else
             {
                 if(lastChar != "+" && lastChar != "-" && lastChar != "*" && lastChar != "/")
                 {
-                    expressionBox.Text += clickedButton.Text;
+                    if(num2 != null)
+                    {
+                        num2 += clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                    }
+                    else
+                    {
+                        expressionBox.Text += clickedButton.Text;
+                    }
                 }
                 else
                 {
-                    expressionBox.Text = clickedButton.Text;
+                    if (elemMath == "+")
+                    {
+                        num2 += clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                        resultBox.Text = (Math.Round((double.Parse(num1) + double.Parse(num2)),5)).ToString();
+                    }else if (elemMath == "-")
+                    {
+                        num2 += clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                        resultBox.Text = (Math.Round((double.Parse(num1) - double.Parse(num2)), 5)).ToString();
+                    }
+                    else if(elemMath == "*")
+                    {
+                        num2 += clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                        resultBox.Text = (Math.Round((double.Parse(num1) * double.Parse(num2)),5)).ToString();
+                    }
+                    else if (elemMath == "/")
+                    {
+                        num2 += clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                        resultBox.Text = (Math.Round((double.Parse(num1) / double.Parse(num2)), 5)).ToString();
+                    }
+                    else
+                    {
+                        num1 += clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                    }
                 }
             }
         }
 
-        private void clickClearBtn(object sender, EventArgs e)
-        {
-            resultBox.Text = "0";
 
-            expressionBox.Text = "0";
 
-            test = "asd";
-        }
+
+
+
+
 
         private void clickOperatorBtn(object sender, EventArgs e)
         {
@@ -60,16 +116,51 @@ namespace Calculator
 
             if (expressionBox.Text.StartsWith("0"))
             {
-                expressionBox.Text = "0";
+                if (clickedButton.Text == "-")
+                {
+                    num1 += clickedButton.Text;
+                    expressionBox.Text = clickedButton.Text;
+                }
             }
             else
             {
                 if (lastChar != "+" && lastChar != "-" && lastChar != "*" && lastChar != "/")
                 {
-                    expressionBox.Text += clickedButton.Text;
-                    resultBox.Text = test;
+                    if(num2 == null)
+                    {
+                        num1 = expressionBox.Text;
+                        elemMath = clickedButton.Text;
+                        expressionBox.Text += clickedButton.Text;
+                    }
+                    else
+                    {
+                        num1 = resultBox.Text;
+                        num2 = "";
+                        elemMath = clickedButton.Text;
+                        expressionBox.Text = "";
+                        expressionBox.Text += resultBox.Text + clickedButton.Text;
+                    }
                 }
             }
         }
+
+
+
+
+
+
+
+        private void clickClearBtn(object sender, EventArgs e)
+        {
+            resultBox.Text = "0";
+            expressionBox.Text = "0";
+            num1 = null;
+            num2 = null;
+        }
+
+
+
+
+
     }
 }
