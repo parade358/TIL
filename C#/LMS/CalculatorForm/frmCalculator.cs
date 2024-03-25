1c2377fe0650da254bbbd74b71634955ccd29cf9
Kevin <parade358@naver.com>
2024-03-25 18:00:29 +0900

    Update_Operation [2024.03.25]

--- C#/LMS/CalculatorForm/frmCalculator.cs
+++ C#/LMS/CalculatorForm/frmCalculator.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CalculatorForm
+namespace CalculatorForm`
 {
     public partial class frmCalculator : Form
     {
