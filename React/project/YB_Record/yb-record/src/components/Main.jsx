a6b7dd81e1d1f1e663308365dc6dc296be5d39a9
Kevin <parade358@naver.com>
2024-04-01 14:23:22 +0900

    Update Main [2024.04.01]

---
+++ React/project/YB_Record/yb-record/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main (){
+
+    return (
+    <>
+        <div>
+            영역 1
+        </div>
+        <div>
+            영역 2
+        </div>
+
+    </>
+    );
+}
