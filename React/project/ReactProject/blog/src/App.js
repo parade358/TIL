b744fdaee44680674ea2aa7bdae4239c1ee208a9
Kevin <parade358@naver.com>
2024-04-10 18:12:44 +0900

    Update ReactProject [2024.04.10]

--- React/project/ReactProject/blog/src/App.js
+++ React/project/ReactProject/blog/src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const post = '신월 짬뽕 맛집';
+  // document.querySelector('h4').innerHTML = post
+  
   return (
     <div className="App">
-      <div>
-        <h4>블로그</h4>
+      <div className="black-nav">
+        <h4 style= {{color : 'red'}} id={post}>블로그</h4>
       </div>
+      <h4>{ post }</h4>
     </div>
   );
 }
