720b0ddf9b61ac6eb4f4a68cae6d206d2bc5d908
Kevin <parade358@naver.com>
2024-04-14 12:36:01 +0900

    Update ReactProject [2024.04.14]

--- React/project/ReactProject/blog/src/App.js
+++ React/project/ReactProject/blog/src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   const post = '신월 짬뽕 맛집';
   // document.querySelector('h4').innerHTML = post
+
+  const [a,b] = useState('남자 코트 추천');
   
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style= {{color : 'red'}} id={post}>블로그</h4>
+        <h4>블로그</h4>
       </div>
       <div className='list'>
         <h4>글제목</h4>
         <p>4월 12일 발행</p>
       </div>
-      <h4>{ post }</h4>
     </div>
   );
 }
