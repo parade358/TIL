a993c339208aac794b1a5e57aa48276b7014cdaa
Kevin <parade358@naver.com>
2024-06-16 17:13:21 +0900

    ATM_QUERY [2024.06.16]

--- MSSQL/Library/ATM/USP_ATM_TEST_L.sql
+++ MSSQL/Library/ATM/USP_ATM_TEST_L.sql
@@ -35,15 +35,15 @@ BEGIN
 	  , res.SAVE_BY		'@@SAVE_BY'
       
 	--UserView
-     , CONVERT(CHAR(10), res.WORK_DATE, 23)				'<ALIGN=CENTER> 일자'
-     , FORMAT(res.START_DTTM, 'HH:mm')					'<ALIGN=CENTER> 시작시각'  
-     , FORMAT(res.END_DTTM, 'HH:mm')					'<ALIGN=CENTER> 종료시각'  
-     , DATEDIFF(MINUTE, res.START_DTTM, res.END_DTTM)	'<ALIGN=CENTER> 소요시간(분)'   
-	 , eq.EQ_NAME										'<ALIGN=CENTER> 설비명'  
-     , res.EQ_MA_DESC									'<ALIGN=CENTER> 조치내용'  
-     , emp.EMP_ID										'<ALIGN=CENTER> 작업자'  
-     , res.SAVE_DTTM									'<ALIGN=CENTER> 저장일시'  
-     , res.SAVE_BY										'<ALIGN=CENTER> 입력자'
+     , STUFF(STUFF(res.WORK_DATE, 5, 0, '-'), 8, 0, '-') '<ALIGN=CENTER> 일자'
+     , FORMAT(res.START_DTTM, 'HH:mm')					 '<ALIGN=CENTER> 시작시각'  
+     , FORMAT(res.END_DTTM, 'HH:mm')					 '<ALIGN=CENTER> 종료시각'  
+     , DATEDIFF(MINUTE, res.START_DTTM, res.END_DTTM)	 '<ALIGN=CENTER> 소요시간(분)'   
+	 , eq.EQ_NAME										 '<ALIGN=CENTER> 설비명'  
+     , res.EQ_MA_DESC									 '<ALIGN=CENTER> 조치내용'  
+     , emp.EMP_ID										 '<ALIGN=CENTER> 작업자'  
+     , res.SAVE_DTTM									 '<ALIGN=CENTER> 저장일시'  
+     , res.SAVE_BY										 '<ALIGN=CENTER> 입력자'
   FROM TB_EQ_MA_RES res WITH(NOLOCK)
   LEFT OUTER JOIN TB_EQ eq on res.EQ_ID = eq.EQ_ID
   LEFT OUTER JOIN TB_EQ_TYPE ty ON eq.EQ_TYPE_ID = ty.EQ_TYPE_ID  
