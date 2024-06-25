82674d1f366ed60e8da8b833e6a0c232e030e477
Kevin <parade358@naver.com>
2024-06-25 10:06:14 +0900

    Update EMP_OVERTIME [2024.06.25]

--- MSSQL/Library/ATM/USP_EMP_OVERTIME_L.sql
+++ MSSQL/Library/ATM/USP_EMP_OVERTIME_L.sql
@@ -25,35 +25,33 @@ BEGIN
     DECLARE @END	VARCHAR(20);  
 
     SET @START	= '19000101';  
-    SET @END	= CONVERT(VARCHAR, CAST(@END_DATE AS DATETIME) + 1, 112);  
-    SET @START	= CASE @P_TERM WHEN 0 THEN @START_DATE  
-                               WHEN 1 THEN CONVERT(VARCHAR, GETDATE(), 112)  
+    SET @START	= CASE @P_TERM WHEN 0 THEN @START_DATE
+                               WHEN 1 THEN CONVERT(VARCHAR, GETDATE(), 112)
                                WHEN 7 THEN CONVERT(VARCHAR, GETDATE() - 6, 112)  
 				  END;  
+	SET @END = CASE @P_TERM WHEN 0 THEN @END_DATE ELSE CONVERT(NVARCHAR,GETDATE(),112) END
     
     SELECT 
 	--ApplyDB
-		   REASON	'@@REASON'
+		   REASON			'@@REASON'
+		 , OVERTIME_DATE	'@@OVERTIME_DATE'
 
 	--UserView
-		 , EMP_NAME																		'사원명'                                          
-         , REASON																		'초과근무사유'
-         , '<ALIGN=CENTER;>'+CONVERT(VARCHAR(10), CAST(START_OVERTIME AS DATE), 120)	'시작시간'
-         , END_OVERTIME																	'종료시간'
-         , TOTAL_OVERTIME																'총 시간'
+		 , EMP_NAME														'<ALIGN=CENTER;>사원명'                                          
+         , REASON														'<ALIGN=CENTER;>초과근무사유'
+		 , OVERTIME_DATE												'<ALIGN=CENTER;>초과근무날짜'
+         , CONVERT(VARCHAR(10), CAST(START_OVERTIME AS DATE), 120)		'<ALIGN=CENTER;>시작시간'
+         , END_OVERTIME													'<ALIGN=CENTER;>종료시간'
+         , TOTAL_OVERTIME												'<ALIGN=CENTER;>총 시간'
          , CASE WHEN OVERTIME_STATUS = 'R'
 				THEN '<align=center;font-weight=bold;color=blue>대기'
                 WHEN OVERTIME_STATUS = 'S'
 				THEN '<align=center;font-weight=bold;color=red>진행'
                 WHEN OVERTIME_STATUS = 'C'
 				THEN '<align=center;font-weight=bold;color=green>완료'
-           END																			'진행상태'
-         , ( SELECT EMP_NAME FROM TB_EMP WITH (NOLOCK) WHERE EMP_ID = CREATE_BY )		'작성자'
-         , CREATE_DTTM																	'작성일시'
-         , ( SELECT EMP_NAME FROM TB_EMP WITH (NOLOCK) WHERE EMP_ID = SAVE_BY )			'수정자'
-         , SAVE_DTTM																	'수정일시'
+           END															'<ALIGN=CENTER;>진행상태'
     FROM TB_EMP_OVERTIME
-    WHERE CREATE_DTTM BETWEEN @START_DATE AND @END_DATE AND ( ISNULL(@OVERTIME_STATUS, '*') = '*' OR OVERTIME_STATUS = @OVERTIME_STATUS )
-
-SET NOCOUNT OFF;
+    WHERE OVERTIME_DATE BETWEEN @START_DATE AND @END_DATE AND OVERTIME_STATUS = @OVERTIME_STATUS
+  
+SET NOCOUNT OFF;  
 END;
