65eb9069312659c6bcd9dd31a91a321e23a7e381
Kevin <parade358@naver.com>
2024-07-03 17:52:17 +0900

    updatge reat [2024.07.03]

--- React/project/ReactProject/blog/src/App.js
+++ React/project/ReactProject/blog/src/App.js
@@ -6,6 +6,19 @@ import { useState } from 'react';
 function App() {
 
   const [title, setTitle] = useState(['1', '2', '3'])
+  const [like, setLike] = useState(0);
+
+  function clickLike(){
+    setLike(like + 1);
+  }
+
+  function clickBtn(){
+    let copy = [...title]
+    console.log(copy)
+    copy[0] = 'test';
+    console.log(copy[0]);
+    setTitle(copy);
+  }
 
   return (
     <div className="App">
@@ -13,17 +26,19 @@ function App() {
         <h4>React</h4>
       </div>
       <div className='list'>
-        <h4>{title[0]}</h4>
+        <h4>{title[0]} <span onClick={ clickLike }>Like</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{title[1]}</h4>
+        <h4>{title[1]} <span onClick={ clickLike }>Like</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{title[2]}</h4>
+        <h4>{title[2]} <span onClick={ clickLike }>Like</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <button onClick={ clickBtn }>Button</button>
     </div>
   );
   
