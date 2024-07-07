482038ae2f6ea21eb7eb46aa4f0b5ca2335a4824
Kevin <parade358@naver.com>
2024-07-07 15:10:23 +0900

    update [2024.07.07]

--- React/project/ReactProject/blog/src/App.js
+++ React/project/ReactProject/blog/src/App.js
@@ -3,22 +3,58 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
 
-  const [title, setTitle] = useState(['1', '2', '3'])
-  const [like, setLike] = useState(0);
+// 컴포넌트 *반복적인 html 축약할때, 큰 페이지들, 자주변경되는것들
+function Modal(){
+  return(
+    <>
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+    </>
+  );
+};
 
-  function clickLike(){
+
+export default function App() {
+
+  const [title, setTitle] = useState(['다', '가', '나']);
+  const [like,  setLike]  = useState(0);
+  const [modal, setModal] = useState(false);
+
+  [1,2,3].map(function(){
+    console.log;
+  })
+
+  function clickLike() {
     setLike(like + 1);
-  }
+  };
 
-  function clickBtn(){
+  function clickBtn() {
     let copy = [...title]
-    console.log(copy)
     copy[0] = 'test';
     console.log(copy[0]);
     setTitle(copy);
-  }
+  };
+
+  function sortArray() {
+    let copy = [...title];
+    copy.sort();
+    setTitle(copy);
+  };
+
+  function clickTitle() {
+    if(modal === false)
+    {
+      setModal(true);
+    } 
+    else
+    {
+      setModal(false);
+    }
+  };
 
   return (
     <div className="App">
@@ -26,21 +62,27 @@ function App() {
         <h4>React</h4>
       </div>
       <div className='list'>
-        <h4>{title[0]} <span onClick={ clickLike }>Like</span> {like} </h4>
+        <h4 onClick={ clickTitle }>{title[0]} <span onClick={ clickLike }>Like</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{title[1]} <span onClick={ clickLike }>Like</span> {like} </h4>
+        <h4 onClick={ clickTitle }>{title[1]} <span onClick={ clickLike }>Like</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{title[2]} <span onClick={ clickLike }>Like</span> {like} </h4>
+        <h4 onClick={ clickTitle }>{title[2]} <span onClick={ clickLike }>Like</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <button onClick={ clickBtn }>Button</button>
+      {
+        //html 작성하는곳이라 if문 불가능, 삼항연산자 활용
+        modal == true ? <Modal/> : null
+      }
+
+
+      <button onClick={ clickBtn }>Button1</button>
+      <button onClick={ sortArray }>Button2</button>
+
     </div>
   );
-  
-} 
-export default App;
+};
