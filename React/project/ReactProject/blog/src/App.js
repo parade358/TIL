815ef9fa545de204d627f522bc3fabac2fc10aac
Kevin <parade358@naver.com>
2024-07-17 14:05:32 +0900

    update [2024.07.17]

--- React/project/ReactProject/blog/src/App.js
+++ React/project/ReactProject/blog/src/App.js
@@ -8,6 +8,7 @@ export default function App() {
   const [title, setTitle] = useState(['다', '가', '나']);
   const [like,  setLike]  = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
+  let flag = 0;
 
   function clickLike(i) {
     let copy = [...like];
@@ -18,16 +19,17 @@ export default function App() {
 
   function clickBtn() {
     let copy = [...title]
-    copy[0] = 'test';
+    copy[0] = 'test'; 
     console.log(copy[0]);
     setTitle(copy);
   };
 
 
-  function clickTitle() {
+  function clickTitle(i) {
     if(modal === false)
     {
       setModal(true);
+      flag = i;
     } 
     else
     {
@@ -45,7 +47,7 @@ export default function App() {
         title.map(function(a, i){
           return (
             <div className='list' key={i}>
-              <h4 onClick={clickTitle}>{title[i]}  <span onClick={() => {
+              <h4 onClick={clickTitle(i)}>{title[i]}  <span onClick={() => {
                                                       let copy = [...like];
                                                       copy[i] = copy[i] + 1;
                                                       setLike(copy);
@@ -58,7 +60,7 @@ export default function App() {
 
       {
         //html 작성하는곳이라 if문 불가능, 삼항연산자 활용
-        modal == true ? <Modal title={title} color='yellow' clickBtn = {clickBtn}/> : null
+        modal == true ? <Modal title={title} color='yellow' clickBtn = {clickBtn} key = {key}/> : null
       }
     </div>
   );
@@ -67,11 +69,10 @@ export default function App() {
 // 컴포넌트 *반복적인 html 축약할때, 큰 페이지들, 자주변경되는것들
 function Modal(props){
 
-
   return(
     <>
     <div className="modal" style={{background : props.color}}>
-        <h4>{props.title[0]}</h4>
+        <h4>{props.title[props.key]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
         <button onClick={ props.clickBtn }>글수정</button>
